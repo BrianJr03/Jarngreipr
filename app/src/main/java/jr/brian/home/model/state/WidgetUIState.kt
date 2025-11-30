d9efbf17a5929fe41c2e6f65125d8006c6c2f2ab
Brian Jr <thaballa79@gmail.com>
2025-11-30 17:27:01 -0500

    only displays edit widget icon in edit mode

--- app/src/main/java/jr/brian/home/model/state/WidgetUIState.kt
+++ app/src/main/java/jr/brian/home/model/state/WidgetUIState.kt
@@ -5,5 +5,6 @@ import jr.brian.home.model.WidgetPage
 data class WidgetUIState(
     val widgetPages: List<WidgetPage> = emptyList(),
     val isInitialized: Boolean = false,
-    val currentPage: Int = 0
+    val currentPage: Int = 0,
+    val editModeByPage: Map<Int, Boolean> = emptyMap()
 )
