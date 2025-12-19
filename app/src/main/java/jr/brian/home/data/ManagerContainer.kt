76f2a09b38983831d9e4d214fafad4cf9cfb479b
Brian Jr <thaballa79@gmail.com>
2025-12-19 08:58:17 -0500

    adds logic and ui

--- app/src/main/java/jr/brian/home/data/ManagerContainer.kt
+++ app/src/main/java/jr/brian/home/data/ManagerContainer.kt
@@ -1,5 +1,18 @@
 package jr.brian.home.data
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import jr.brian.home.ui.theme.managers.LocalAppDisplayPreferenceManager
+import jr.brian.home.ui.theme.managers.LocalAppPositionManager
+import jr.brian.home.ui.theme.managers.LocalAppVisibilityManager
+import jr.brian.home.ui.theme.managers.LocalGridSettingsManager
+import jr.brian.home.ui.theme.managers.LocalHomeTabManager
+import jr.brian.home.ui.theme.managers.LocalIconPackManager
+import jr.brian.home.ui.theme.managers.LocalOnboardingManager
+import jr.brian.home.ui.theme.managers.LocalPageCountManager
+import jr.brian.home.ui.theme.managers.LocalPageTypeManager
+import jr.brian.home.ui.theme.managers.LocalPowerSettingsManager
+import jr.brian.home.ui.theme.managers.LocalWidgetPageAppManager
 import javax.inject.Inject
 
 /**
@@ -18,3 +31,22 @@ data class ManagerContainer @Inject constructor(
     val pageTypeManager: PageTypeManager,
     val iconPackManager: IconPackManager
 )
+
+@Composable
+fun ManagerContainer.ManagerCompositionLocalProvider(content: @Composable () -> Unit) {
+    CompositionLocalProvider(
+        LocalAppVisibilityManager provides appVisibilityManager,
+        LocalGridSettingsManager provides gridSettingsManager,
+        LocalAppDisplayPreferenceManager provides appDisplayPreferenceManager,
+        LocalPowerSettingsManager provides powerSettingsManager,
+        LocalWidgetPageAppManager provides widgetPageAppManager,
+        LocalHomeTabManager provides homeTabManager,
+        LocalOnboardingManager provides onboardingManager,
+        LocalAppPositionManager provides appPositionManager,
+        LocalPageCountManager provides pageCountManager,
+        LocalPageTypeManager provides pageTypeManager,
+        LocalIconPackManager provides iconPackManager
+    ) {
+        content()
+    }
+}
