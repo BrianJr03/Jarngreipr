b319ba07a2240495dcc3a9c4e135fd083edd8d52
Brian Jr <thaballa79@gmail.com>
2025-12-21 17:52:39 -0500

    code cleanup

--- app/src/main/java/jr/brian/home/JarngreiprApplication.kt
+++ app/src/main/java/jr/brian/home/JarngreiprApplication.kt
@@ -2,6 +2,12 @@ package jr.brian.home
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import jr.brian.home.util.CrashLogger
 
 @HiltAndroidApp
-class JarngreiprApplication : Application()
+class JarngreiprApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        CrashLogger.initialize(this)
+    }
+}
