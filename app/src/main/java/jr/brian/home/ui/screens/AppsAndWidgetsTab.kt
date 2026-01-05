c2ccec327e5da2736a19eccfdc9c545c3556a803
Brian Jr <thaballa79@gmail.com>
2026-01-05 12:05:25 -0500

    increases max page count to 7

--- app/src/main/java/jr/brian/home/ui/screens/AppsAndWidgetsTab.kt
+++ app/src/main/java/jr/brian/home/ui/screens/AppsAndWidgetsTab.kt
@@ -278,7 +278,6 @@ fun AppsAndWidgetsTab(
         HomeTabSelectionDialog(
             currentTabIndex = currentHomeTabIndex,
             totalPages = totalPages,
-            allApps = allApps,
             onTabSelected = { index ->
                 homeTabManager.setHomeTabIndex(index)
             },
