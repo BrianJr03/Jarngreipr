83fc3d79dd402443202eb64f5eb637fc4cbc23b5
Brian Jr <thaballa79@gmail.com>
2026-01-16 14:22:59 -0500

    updates folder support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 33
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0.4"
+        versionName = "1.0.4h"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
