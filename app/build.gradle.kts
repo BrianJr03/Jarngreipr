d1f7556482e077b553650ac563a736b2f6a45a75
Brian Jr <thaballa79@gmail.com>
2026-01-19 18:37:01 -0500

    adds patch notes & bump build version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 33
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0.4h"
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
