a63cb36cca101679f28bd212e5c3a537136bdfd2
Brian Jr <thaballa79@gmail.com>
2026-03-08 10:52:44 -0400

    adds automation support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 33
         targetSdk = 36
         versionCode = 1
-        versionName = "1.8.4"
+        versionName = "1.9"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
